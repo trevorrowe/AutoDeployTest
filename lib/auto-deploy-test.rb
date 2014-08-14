6482d25b97608f02ef99cee8b1808a1703e66d17
Trevor Rowe <trevorrowe@gmail.com>
2014-08-14 15:22:50 -0700

    Increased documentation coverage.

--- lib/auto-deploy-test.rb
+++ lib/auto-deploy-test.rb
@@ -3,6 +3,7 @@ require 'auto_deploy_test/version'
 # Nothing to see here.
 class AutoDeployTest
 
+  # @return [Boolean] Returns `true`.
   def testable_method
     true
   end
--- lib/auto_deploy_test/version.rb
+++ lib/auto_deploy_test/version.rb
@@ -1,3 +1,4 @@
 class AutoDeployTest
+  # gem version
   VERSION = '0.1.0'
 end
