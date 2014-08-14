13aefeee3b8a7f260a674cec4be01330b238f8ce
Trevor Rowe <trevorrowe@gmail.com>
2014-08-14 15:44:55 -0700

    Improved test coverage.

--- spec/auto_deploy_test_spec.rb
+++ spec/auto_deploy_test_spec.rb
@@ -6,4 +6,8 @@ describe AutoDeployTest do
     expect(AutoDeployTest::VERSION).to match(/^\d+\.\d+\.\d+$/)
   end
 
+  it 'returns true from #testable_method' do
+    expect(AutoDeployTest.new.testable_method).to be(true)
+  end
+
 end
